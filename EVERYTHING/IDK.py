6687f15f3f88508d91233d9f474613b336c4fec1
Soumyajit Das <153814779+soumyajitxedu@users.noreply.github.com>
2025-10-28 23:10:55 +0530

    idk.py

---
+++ EVERYTHING/IDK.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np 
+
+x = np.array([1,2,3,4,5,6,7])
+y = np.array([12,32,112,3,4,5,6])
+plt.plot(x,y, marker =".",
+         markersize=10)
+
+
+
+plt.show()
