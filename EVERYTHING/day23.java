9853a6ba01e5367dafe8dc2743d1ede58add7b04
Soumyajit Das <153814779+soumyajitxedu@users.noreply.github.com>
2025-12-31 21:16:52 +0530

    Add files via upload

---
+++ EVERYTHING/day23.java
@@ -0,0 +1,13 @@
+class day23
+{
+    public static void main(String[] args) throws InterruptedException {
+       int start = 10;
+       for (int i = start; i > 0; i--) {
+        System.out.println(i);
+        Thread.sleep(1000);
+           
+       }
+       System.out.println("happy new year");
+        
+    }
+}
