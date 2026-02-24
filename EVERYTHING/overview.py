eec59c75ef24c435853c65633950bbd3c13db20d
Soumyajit Das <153814779+soumyajitxedu@users.noreply.github.com>
2026-02-24 20:43:51 +0530

    Add files via upload

--- EVERYTHING/overview.py
+++ EVERYTHING/overview.py
@@ -1,4 +1,13 @@
-import torch 
-a = torch.tensor([[1,218,725],
-                  [51,44,243]])
-print(a[1,2])
+import torch
+x = torch.tensor([1.,2.,3.,4.])
+yt = torch.tensor([2.,4.,6.,8.])
+w =torch.tensor(0.0,requires_grad=True)
+lr = 0.1
+epochs = 10
+for epoch in range(epochs):
+    yp = (w * x)
+    loss = ((yp -yt)**2).mean()
+    loss.backward()
+    with torch.no_grad():
+        w -= lr * w.grad
+    print(f"epoch{epoch}: w = {w.item():.4f}, loss = {loss.item():.4f}")
