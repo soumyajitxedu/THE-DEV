c45a43312dabe3e6c58ae85a38061976f0cfc21e
Soumyajit Das <153814779+soumyajitxedu@users.noreply.github.com>
2026-05-01 23:03:48 +0530

    Add files via upload

---
+++ EVERYTHING/storge_cloud.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class storage_cloud{
+    int anco;
+    int space;
+    double price;
+
+    void accept(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enetr the anco : ");
+        anco = sc.nextInt();
+        System.out.println("enter the space in gb: ");
+        space = sc.nextInt();
+
+    }
+
+    void calculate(){
+        if 
+    }
+
+}
